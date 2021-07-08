1031c572b5de36d1c2958068aca4d21a44843be9
Kartikeyavasthi17 <71058480+Kartikeyavasthi17@users.noreply.github.com>
2021-07-08 23:19:14 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,65 @@
+class Form
+{
+    constructor(){
+
+        this.title =createElement('h2');
+        this.input =createInput("NAME");
+        this.button =createButton('PLAY');
+        this.reset =createButton('Reset');
+        this.greeting =createElement('h3');
+    }
+
+    hide()
+    {
+        this.title.hide();
+        this.input.hide();
+        this.button.hide();
+        this.greeting.hide();
+    }
+  
+    display()
+    {
+        this.title.html("FRUIT CATCHER");
+        this.title.position(350, 50);
+        this.title.style('font-size', '70px');
+        this.title.style('color', 'skyblue');
+        this.input.position(550,400);
+        this.input.style('width', '200px');
+        this.input.style('height', '20px');
+        this.input.style('background', 'lavender');
+        this.button.position(560,500);
+        this.button.style('width', '200px');
+        this.button.style('height', '40px');
+        this.button.style('background', 'lightpink');
+        this.reset.position(900, 660);
+        this.reset.style('width', '100px');
+        this.reset.style('height', '30px');
+        this.reset.style('background', 'lightpink');
+
+        this.reset.mousePressed(()=>{
+            game.update(0);
+            player.updateCount(0);
+        }
+        )
+
+        this.button.mousePressed(()=>{
+            this.input.hide();
+            this.button.hide();
+
+            player.name =this.input.value();
+            playerCount =playerCount+ 1;
+            player.index =playerCount;
+
+            player.updateCount(playerCount);
+
+            player.update();
+ 
+            this.greeting.html("Hello" + player.name)
+            this.greeting.position(displayWidth/2-70,displayHeight/4);
+        })
+        
+        
+    }
+
+
+}
